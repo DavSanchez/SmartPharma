24487530c93cf775a66e3a2c8bf95c646f3169b8
DavSanchez <d.vinternatt@gmail.com>
2016-11-14 22:32:55 +0000

    Interface development. Segues done and programmed. Project builds but main screen blackouts.

---
+++ SmartPharma/MenuPrincipalCollectionViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MenuPrincipalCollectionViewController.swift
+//  SmartPharma
+//
+//  Created by David on 14/11/2016.
+//  Copyright © 2016 David. All rights reserved.
+//
+
+import UIKit
+
+class MenuPrincipalCollectionViewController: UICollectionViewController {
+    
+    @IBAction func botonFarmas(sender: UIButton) {
+        self.performSegueWithIdentifier("muestraBuscaFarmas", sender: self)
+    }
+    
+    @IBAction func botonTratamientos(sender: UIButton) {
+        // Sin implementar.
+    }
+    
+    @IBAction func botonUsuarios(sender: UIButton) {
+        self.performSegueWithIdentifier("muestraVistaUsuarios", sender: self)
+    }
+    
+    @IBAction func botonInfoHelp(sender: UIButton) {
+        self.performSegueWithIdentifier("muestraInfoAyuda", sender: self)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
