909d15d08a9efbe5563e9232016f1730efaa3c00
DavSanchez <d.vinternatt@gmail.com>
2016-11-19 14:16:34 +0000

    Implemented Map Location Manager.

--- SmartPharma/BuscaFarmasViewController.swift
+++ SmartPharma/BuscaFarmasViewController.swift
@@ -7,13 +7,34 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class BuscaFarmasViewController: UIViewController {
+class BuscaFarmasViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+        
+        // Es su propio delegado
+        self.locationManager.delegate = self
+        
+        // La precisión de la ubicación es la mejor posible
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        // Solo queremos usar la ubicación mientras se use la app, no en segundo plano
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        // "Enciende" el localizador
+        self.locationManager.startUpdatingLocation()
+        
+        // Pone la ubicación en mapView
+        self.mapView.showsUserLocation = true
     }
     
     override func didReceiveMemoryWarning() {
@@ -21,7 +42,32 @@ class BuscaFarmasViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: - Métodos del delegado de ubicación
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        // Coge siempre la ubicación más reciente
+        let location = locations.last
+        
+        // Busca el centro de dicha ubicación
+        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        
+        // Crea la región circular en nuestra ubicación. El span es el zoom, puesto a 1 para el mayor zoom posible
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+        
+        // Establece la región en el mapView con la animación del zoom
+        self.mapView.setRegion(region, animated: true)
+        
+        // Una vez tiene la ubicación representada no hace falta estar actualizando
+        self.locationManager.stopUpdatingLocation()
+    }
     
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        
+        // Saca errores para depuración
+        print("Errores: " + error.localizedDescription)
+    }
+    
+
     /*
      // MARK: - Navigation
      
